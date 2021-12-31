72216ea3e735ca4089a20918ed23d8a2eea42f8a
Alvaro Fierro <business.afierro@gmail.com>
2021-12-30 21:51:42 -0600

    Exersices done

--- 01_arrays/exersices.js
+++ 01_arrays/exersices.js
@@ -1,25 +1,23 @@
-// EXERSICE 1
 console.log("------------ EXERSICE 1 ------------");
 
 // Create an array of three items whatever you want.
 // Add it to a variable called myArray
 
-// let myArray = ["Computer", "Laptop", "Phone"];
-// console.log(myArray);
+let myArray = ["Computer", "Laptop", "Phone"];
+console.log(myArray);
 
-// // Modify the first two items with bracket notation
+// Modify the first two items with bracket notation
 
-// myArray[0] = "Keyboard";
-// myArray[1] = "Monitor";
+myArray[0] = "Keyboard";
+myArray[1] = "Monitor";
 
-// console.log(myArray);
+console.log(myArray);
 
-// // Finnaly add a new item to the beginning of the array
+// Finnaly add a new item to the beginning of the array
 
-// myArray.unshift("Chair");
-// console.log(myArray);
+myArray.unshift("Chair");
+console.log(myArray);
 
-// EXERSICE 2
 console.log("------------ EXERSICE 2 ------------");
 
 // Convert the next string into an array
@@ -29,7 +27,7 @@ console.log("------------ EXERSICE 2 ------------");
 let myString = "Ryu+Ken+Chun-Li+Cammy+Guile+Sakura+Sagat+Juri";
 let myArray = myString.split("+");
 
-// Store the length of the array in a varibale called arrayLength
+// Store the length of the array in a variable called arrayLength
 
 let arrayLength = myArray.length;
 console.log("The length of the array is:", arrayLength);
@@ -38,22 +36,64 @@ console.log("The length of the array is:", arrayLength);
 
 let lastItem = myArray[myArray.length - 1];
 console.log("The last item is:", lastItem);
-
 console.log(myArray);
 
-let frutas = [
-  "Manzana",
-  "Jugo",
-  "Computadora",
-  "Teclado",
-  "Raton",
-  "RHCP",
-  "Sacrifice",
-  "Listen",
-  "Trick",
-];
-console.log(frutas);
-console.log(frutas.length);
+console.log("------------ EXERSICE 3 ------------");
 
-let eliminar = frutas.splice(5, 2); // Posicion, numElementos
-console.log(eliminar);
+// For this array task, we provide you with a starting array and you will work
+// in somewhat the oppostie direction. You need too:
+
+// 1. Remove the last item in the array
+
+let myArray = [
+  "Ryu",
+  "Ken",
+  "Chun-Li",
+  "Cammy",
+  "Guile",
+  "Sakura",
+  "Sagat",
+  "Juri",
+];
+console.log(myArray);
+console.log(`Last item deleted:`, myArray.pop());
+
+// 2. Add two new names to the end of the array
+
+myArray.push("Item 1", "Item 2");
+console.log(myArray);
+
+// 3. Go over each item in the array and add its index number after the name inside parentheses for example Ryu (0).
+
+let numberAfterName = (name, index) => {
+  return `${name} (${index})`;
+};
+
+console.log(myArray.map(numberAfterName));
+
+// 4. Finally, join the array items together in a single string called myString, with a separator of "-"
+
+let myString = myArray.join("-");
+console.log(myString);
+
+console.log("------------ EXERSICE 4 ------------");
+
+// For this array task, we provide you with a starting array
+// listing the names of some birds. You have to:
+
+// 1. Find the index of "Eagles" item, and use that to remove the "Eagles" item
+const birds = ["Parrots", "Falcons", "Eagles", "Emus", "Caracaras", "Egrets"];
+
+const deleteEagles = (array) => {
+  let index = array.indexOf("Eagles");
+  array.splice(index, 1);
+  return array;
+};
+
+console.log(deleteEagles(birds));
+
+// 2. Make a new array called eBirds, that contains only bird from the original
+// whose names begin with "E".
+
+const eBirds = birds.filter((bird) => bird.startsWith("E"));
+console.log(eBirds);
